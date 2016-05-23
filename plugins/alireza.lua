1d6f0faa04d6c8b1fd80b0e2156223609c74d2b5
Rezasamadifar1395 <rezasamadifar1395@gmail.com>
2016-05-23 00:35:51 -0700

    Add files via upload

---
+++ plugins/alireza.lua
@@ -0,0 +1,64 @@
+https://github.com/RememberTheAir/GroupButler.git
+
+انتی اسپم
+https://github.com/SEEDTEAM/jack-telegram-bot.git
+
+جک
+https://github.com/SEEDTEAM/file-manager-bot.git
+
+فایل منیجر
+https://github.com/SEEDTEAM/TelegramLoggingBot.git
+
+متصل کننده ۲گروه
+https://github.com/amir-sereen/lua-api-bot.git
+
+عکس به استیکر بولد ایتالیک هایپر استیکر به عکس
+https://github.com/neoranger/NeoBot.git
+
+تفریحی
+https://github.com/eternnoir/pyTelegramBotAPI.git
+
+سورس api کامل مناسب برنامه نویسا
+https://github.com/Tronikart/RadRetroRobot.git
+
+تفریحی کامل
+https://github.com/i32ropie/lol.git
+
+روبات بازی league of legends که چندین زبان داره و خیلی سرگرم کنندس
+https://github.com/mrd0ll4r/rock-paper-scissors-bot.git
+
+سنگ کاغذ قیچی
+https://github.com/Botnexxx/payamresan.git
+
+پیامرسان
+https://github.com/ThisIsArman/polaris.git
+
+تفریحی
+https://github.com/ThisIsArman/telegram-iranbot.git
+
+ترجمه فارسی به فارسی و انگلیسی به فارسی
+https://github.com/thisismehrpouya/GPMod.git
+
+ربات ایدی و اینفو
+https://github.com/IDeactive/PinkPantherBot.git
+
+پینک پنتر
+https://github.com/topkecleon/otouto.git
+
+اوتوتو
+https://github.com/ArashEmpix/Elemental.git
+
+بات رای گیری
+https://github.com/ArashEmpix/Echo-Bot.git
+
+سورس بات کسگو(اکو)
+https://github.com/LegendTEAM/moon-api-bot.git
+
+مون بات
+https://github.com/Rondoozle/MasterAssistant.git
+
+سخنگو انگلیسی
+https://github.com/ferisystem/SuzzyBot.git
+
+سوزی بات
+
