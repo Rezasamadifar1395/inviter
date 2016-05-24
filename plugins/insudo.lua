7731c71099417ba78d2184cdaa962e00c76f5322
Rezasamadifar1395 <rezasamadifar1395@gmail.com>
2016-05-24 01:13:49 -0700

    Create insudo.lua

---
+++ plugins/insudo.lua
@@ -0,0 +1,36 @@
+do
+
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+
+local function run(msg, matches)
+  local user =  146393436
+
+  if matches[1] == "insudo" then
+    user = 'user#id'..user
+  end
+
+  — The message must come from a chat group
+  if msg.to.type == 'chat' then
+    local chat = 'channel#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
+    return "inviting sudo......"
+  else 
+    return 'This isnt a chat group!'
+  end
+
+end
+
+return {
+  description = "insudo", 
+  usage = {
+    "!insudo"
+  patterns = {
+    "^[!/](insudo)$"
+  }, 
+  run = run 
+}
+
+end
